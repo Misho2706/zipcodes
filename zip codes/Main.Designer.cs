b525df4028330ca5fdeee0121ba2bdff1771f9e9
mishotsiskaridze <mishotsiskaridze@gmail.com>
2023-09-15 16:29:32 +0400

    Add project files.

---
+++ zip codes/Main.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace zip_codes
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(432, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(97, 170);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(345, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(420, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(500, 170);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(206, 21);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(297, 260);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(668, 407);
+            this.listBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(671, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Randomize";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1315, 699);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ zip codes/Main.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zip_codes
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            readfile();
+        }
+        char[] letters = new char[26] {'a','b','c','d','e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z' };
+        List<int> indexes = new List<int>();
+        Random numbergenerator = new Random();
+
+        class ZipCodeMasks
+        {
+        public  ZipCodeMasks(string country, string mask)
+            {
+                Country = country;
+                Mask = mask;
+            }
+          public  string Country { get; set; }
+          public  string Mask { get; set; }
+        }
+        List<string> countries = new List<string>();
+        List<string> masks = new List<string>();
+
+        void readfile()
+        {
+            string file = @"C:\Users\1\Downloads\zipcodemasks.txt";
+            StreamReader fileReader = new StreamReader(file);
+            string line1;
+            string line2;
+            while ((line1 = fileReader.ReadLine()) != null)
+            {
+                line2 = fileReader.ReadLine();
+                ZipCodeMasks data = new ZipCodeMasks(line1, line2);
+                countries.Add(data.Country);
+                masks.Add(data.Mask);
+            }
+
+            comboBox1.DataSource = countries.Distinct().ToList();
+        }
+
+        void addNewZipCode(string zipCode, string zipMask)
+        {
+            if (zipCode.Length != zipMask.Length)
+            {
+                MessageBox.Show("please enter code correctly");
+                return;
+            }
+            for (int i = 0; i < zipCode.Length; i++)
+            {
+                if (zipMask[i] != '9' && zipMask[i] != 'A' && zipCode[i] != zipMask[i])
+                {
+                    MessageBox.Show("please enter code correctly");
+                    return;
+                }
+                if (zipMask[i] == '9' && !char.IsDigit(zipCode[i]))
+                {
+                    MessageBox.Show("please enter code correctly");
+                    return;
+                }
+
+                if (zipMask[i] == 'A' && !char.IsLetter(zipCode[i]))
+                {
+                    MessageBox.Show("please enter code correctly");
+                    return;
+                }
+            }
+            string result = comboBox1.SelectedItem.ToString() + " " + zipCode.ToUpper();
+            listBox1.Items.Add(result);
+        }
+
+        string RandomZipCode (string zipMask)
+        {
+            string result = "";
+            for (int i = 0; i < zipMask.Length; i++)
+            {
+                if (zipMask[i] == '9')
+                {
+                    result += numbergenerator.Next(0, 10);
+                }
+                
+
+                if (zipMask[i] == 'A')
+                {
+                    result += letters[numbergenerator.Next(0, 26)];
+                }
+                if (zipMask[i] != '9' && zipMask[i] != 'A')
+                {
+                    result += zipMask[i];
+                }    
+            }
+            return result;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string zipCode = textBox1.Text;
+            textBox1.Clear();
+            string zipMask = comboBox2.SelectedItem.ToString();
+            addNewZipCode(zipCode, zipMask);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();
+            for (int i = 0; i < countries.Count(); i++)
+            {
+                if (comboBox1.SelectedItem.ToString() == countries[i])
+                {
+                    indexes.Add(i);
+                }
+            }
+            
+            for (int j = 0; j < indexes.Count() ; j++)
+            {
+                comboBox2.Items.Add(masks[indexes[j]]);
+                comboBox2.SelectedIndex = 0;
+            }
+            indexes.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string zipMask = comboBox2.SelectedItem.ToString();
+            string result = comboBox1.SelectedItem.ToString() + " " + RandomZipCode(zipMask).ToUpper();
+           listBox1.Items.Add(result);
+        }
+    }
+}
